04362b787aac5295cb90a64c49fa86df31d39cdc
mohamed-abdina <abdinasirmohamed@zetech.ac.ke>
2026-07-05 11:47:17 -0700

    Update Django reservation app

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('function/', views.hello_world),
+    path('', views.home, name='home'),
+    path('function/', views.hello_world, name='hello_world'),
+    path('reservation/', views.reservation, name='reservation'),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,24 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ReservationForm
+
 
 def home(request):
     return HttpResponse("Home Page")
 
+
 def hello_world(request):
     return HttpResponse("Hello World")
+
+
+def reservation(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success")
+
+    return render(request, 'reservation.html', {'form': form})
